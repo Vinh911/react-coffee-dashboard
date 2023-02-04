3a289294519df7ae7c500ecdcaeef9dea4d07b98
vnguyen <vnguyen@queo-group.com>
2023-02-04 16:39:47 +0100

    header placeholder

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+        <h1>React Coffee Dashboard</h1>
+        </div>
+    );
+};
+
+export default Header;
